435928d33fd0a3f22695a355cb6112b3f614dcc4
Sistone69 <75392004+Sistone69@users.noreply.github.com>
2021-03-18 21:51:17 +0100

    Pensavi posse un modefica e invece ero io DIO!

---
+++ commands/other/hentai.js
@@ -0,0 +1,22 @@
+const { Command } = require('../../discord.js-commando/src');
+
+module.exports = class RandomNumberCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'hentai',
+      aliases: ['hentai;dio'],
+      memberName: 'hentai',
+      group: 'other',
+      description: 'Invia una hentai'
+    });
+  }
+
+  run(message) {
+    message.channel.send({
+        files: [{
+            attachment: "https://raw.githubusercontent.com/spartacus04/Vaiolowser/master/resources/images/Dio.jpeg",
+            name: 'Dio.jpeg'
+        }]
+    })
+  }
+};
