9e54b2dba0dcf3beb50b6e97ba6cda85e95d5273
Sistone69 <75392004+Sistone69@users.noreply.github.com>
2021-03-28 16:44:07 +0200

    adios
    un archeokoko in piu è solo meglio

---
+++ commands/other/adios.js
@@ -0,0 +1,22 @@
+const { Command } = require('../../discord.js-commando/src');
+
+module.exports = class RandomNumberCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'adios',
+      aliases: ['adios;archeokoko'],
+      memberName: 'adios',
+      group: 'other',
+      description: 'Invia un archeokoko che ti saluta'
+    });
+  }
+
+  run(message) {
+    message.channel.send({
+        files: [{
+            attachment: "https://raw.githubusercontent.com/spartacus04/Vaiolowser/master/resources/gifs/archokoko.gif",
+            name: 'archokoko.gif'
+        }]
+    })
+  }
+};
