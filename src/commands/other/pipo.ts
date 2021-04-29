a7bdd81bf1e4a2f2e88192884e2da9dc3bb7bfb2
spartacus04 <52862005+spartacus04@users.noreply.github.com>
2021-04-29 12:01:53 +0200

    pipo

---
+++ src/commands/other/pipo.ts
@@ -0,0 +1,28 @@
+import { CommandoClient, CommandoMessage, Command } from 'discord.js-commando-it';
+
+module.exports = class SayCommand extends Command {
+  constructor(client : CommandoClient) {
+    super(client, {
+      name: 'pipo',
+      aliases: ['pipo'],
+      memberName: 'pipo',
+      group: 'other',
+      description: 'pipo',
+      args: [
+        {
+          key: 'lenght',
+          prompt: 'noh',
+          type: 'integer'
+        }
+      ]
+    });
+  }
+
+  run(message : CommandoMessage, { lenght } : { lenght : number }) {
+    var p : string = "";
+    for (let i = 0; i < lenght; i++) {
+      p += "=";
+    }
+    return message.say("8" + p + ">");
+  }
+};
