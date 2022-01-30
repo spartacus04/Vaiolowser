b3bbc06b7fdc48a28fdb5b6f0a451e61eddce3ee
spartacus04 <52862005+spartacus04@users.noreply.github.com>
2022-01-30 21:48:02 +0100

    Revert "aggiorno dipendenze"
    This reverts commit 9ae1ef26565bd90716af944892efdd01cb5648a0.
    
    	modified:   package.json
    	modified:   src/util/games/input.ts
    	modified:   src/util/games/util.ts

--- src/util/games/input.ts
+++ src/util/games/input.ts
@@ -1,18 +1,18 @@
-import { CollectorFilter, Interaction, Message } from 'discord.js';
+import { CollectorFilter, Message, MessageComponentInteraction } from 'discord.js';
 
 export const singleButtonInput = (message : Message, ...allowedUsers : string[]) : Promise<string> => {
 	return new Promise<string>((resolve) => {
-		const filter : CollectorFilter<[Interaction]> = i =>
+		const filter : CollectorFilter<[MessageComponentInteraction]> = i =>
 			(allowedUsers.length != 0 ? allowedUsers.includes(i.user.id) : true);
 
-		const collector = message.createMessageComponentCollector({ componentType: 'BUTTON', filter, max: 1, time: 15000 });
+		const collector = message.channel.createMessageComponentCollector({ filter, message, max: 1, time: 15000 });
 
 		collector.on('collect', i => {
-			resolve(i.id);
+			resolve(i.customId);
 		});
 
 		collector.on('end', i => {
-			resolve(i.size > 0 ? i.first().id : '-1');
+			resolve(i.size > 0 ? i.first().customId : '-1');
 		});
 	});
 };
